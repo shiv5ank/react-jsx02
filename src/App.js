442e7b58e2d473cc779472c975549d0af29ccd79
shiv5ank <Shivank0596@gmail.com>
2023-08-06 08:36:12 +0530

    jsx02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo/airbnb.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>
+          The Evolution of
+          <br />
+          Airbnb's Frontend
+        </h1>
+        <p>@spikebrehm</p>
+        <div className="logo-container">
+          <img src={logo} alt="" height={64}></img>
+          <h4>airbnb</h4>
+        </div>
+      </div>
     </div>
   );
 }
